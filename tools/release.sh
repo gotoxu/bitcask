0c8b62c0bcbd859af6f970a01fc6716b59b1bbb3
James Mills <1290234+prologic@users.noreply.github.com>
2020-01-25 15:22:21 +1000

    Use prologic/action-remark-lint@v0.0.3

--- tools/release.sh
+++ tools/release.sh
@@ -5,12 +5,15 @@ VERSION="$(git describe --abbrev=0 --tags)"
 VERSION=${VERSION:-'0.0.0'}
 
 # Get number parts
-MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
-MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
-PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MAJOR="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"
+VERSION="${VERSION#*.}"
 
 # Increase version
-PATCH=$((PATCH+1))
+PATCH=$((PATCH + 1))
 
 TAG="${1}"
 
